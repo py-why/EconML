88d3a3d101d98dd21296b696c0e9edb2f1be239e
Vasilis <vasy@microsoft.com>
2019-03-15 13:35:20 -0400

    removed cython stuff

--- econml/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(
-    ext_modules = cythonize("causal_tree_node.pyx"),
-    include_dirs=[numpy.get_include()]
-)
