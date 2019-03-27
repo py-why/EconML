23886aceb9ae59a3033ea9b9e5510274c1add23f
Keith Battocchi <kebatt@microsoft.com>
2019-03-27 11:42:45 -0400

    Add test to run each notebook, reporting all errors

---
+++ econml/tests/test_notebooks.py
@@ -0,0 +1,30 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import pytest
+import html
+import os
+import nbformat
+import nbconvert
+
+_nbdir = os.path.join(os.path.dirname(__file__), '..', '..', 'notebooks')
+_notebooks = [path
+              for path in os.listdir(_nbdir)
+              if path.endswith('.ipynb')]
+
+
+@pytest.mark.parametrize("file", _notebooks)
+@pytest.mark.notebook
+def test_notebook(file):
+    nb = nbformat.read(os.path.join(_nbdir, file), as_version=4)
+    # require all cells to complete within 15 minutes, which will help prevent us from
+    # creating notebooks that are annoying for our users to actually run themselves
+    ep = nbconvert.preprocessors.ExecutePreprocessor(timeout=900, allow_errors=True)
+    ep.preprocess(nb, {'metadata': {'path': _nbdir}})
+    errors = [nbconvert.preprocessors.CellExecutionError.from_cell_and_msg(cell, output)
+              for cell in nb.cells if "outputs" in cell
+              for output in cell["outputs"]
+              if output.output_type == "error"]
+    if errors:
+        err_str = "\n".join(html.unescape(str(err)) for err in errors)
+        raise AssertionError("Encountered {0} exception(s):\n{1}".format(len(errors), err_str))
