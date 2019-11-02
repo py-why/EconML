51728b9252af17d08a9186aef6e1df5bd284c192
Vasilis <vasy@microsoft.com>
2019-11-01 22:09:22 -0400

    linting style for test_ortho_learner.py

--- econml/tests/test_ortho_learner.py
+++ econml/tests/test_ortho_learner.py
@@ -14,12 +14,16 @@ import joblib
 class TestOrthoLearner(unittest.TestCase):
 
     def test_crossfit(self):
+
         class Wrapper:
+
             def __init__(self, model):
                 self._model = model
+
             def fit(self, X, y, W=None):
                 self._model.fit(X, y)
                 return self
+
             def predict(self, X, y, W=None):
                 return self._model.predict(X), y - self._model.predict(X), X
 
@@ -30,11 +34,11 @@ class TestOrthoLearner(unittest.TestCase):
         nuisance, model_list= _crossfit(Wrapper(model),
                                         folds,
                                         X, y, W=y, Z=None)
-        np.testing.assert_allclose(nuisance[0][folds[0][1]], model.fit(X[folds[0][0]], y[folds[0][0]]).predict(X[folds[0][1]]))
-        np.testing.assert_allclose(nuisance[0][folds[0][0]], model.fit(X[folds[0][1]], y[folds[0][1]]).predict(X[folds[0][0]]))
+        np.testing.assert_allclose(nuisance[0][folds[0][1]],
+                                   model.fit(X[folds[0][0]], y[folds[0][0]]).predict(X[folds[0][1]]))
+        np.testing.assert_allclose(nuisance[0][folds[0][0]],
+                                   model.fit(X[folds[0][1]], y[folds[0][1]]).predict(X[folds[0][0]]))
 
         coef_ = np.zeros(X.shape[1])
         coef_[0] = 1
         [np.testing.assert_allclose(coef_, mdl._model.coef_, rtol=0, atol=0.08) for mdl in model_list]
-    
-    
