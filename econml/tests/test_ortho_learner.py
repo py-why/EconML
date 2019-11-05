17f35323f083fff86b574acad875a8c6258b7c78
Vasilis <vasy@microsoft.com>
2019-11-04 21:02:48 -0500

    added some more tests related to input None's in _crossfit

--- econml/tests/test_ortho_learner.py
+++ econml/tests/test_ortho_learner.py
@@ -21,11 +21,11 @@ class TestOrthoLearner(unittest.TestCase):
             def __init__(self, model):
                 self._model = model
 
-            def fit(self, X, y, W=None):
+            def fit(self, X, y, Q, W=None):
                 self._model.fit(X, y)
                 return self
 
-            def predict(self, X, y, W=None):
+            def predict(self, X, y, Q, W=None):
                 return self._model.predict(X), y - self._model.predict(X), X
 
         np.random.seed(123)
@@ -35,16 +35,61 @@ class TestOrthoLearner(unittest.TestCase):
         model = Lasso(alpha=0.01)
         nuisance, model_list, fitted_inds = _crossfit(Wrapper(model),
                                                       folds,
-                                                      X, y, W=y, Z=None)
+                                                      X, y, y, W=y, Z=None)
         np.testing.assert_allclose(nuisance[0][folds[0][1]],
                                    model.fit(X[folds[0][0]], y[folds[0][0]]).predict(X[folds[0][1]]))
         np.testing.assert_allclose(nuisance[0][folds[0][0]],
                                    model.fit(X[folds[0][1]], y[folds[0][1]]).predict(X[folds[0][0]]))
-
         coef_ = np.zeros(X.shape[1])
         coef_[0] = 1
         [np.testing.assert_allclose(coef_, mdl._model.coef_, rtol=0, atol=0.08) for mdl in model_list]
         np.testing.assert_array_equal(fitted_inds, np.arange(X.shape[0]))
+
+        np.random.seed(123)
+        X = np.random.normal(size=(5000, 3))
+        y = X[:, 0] + np.random.normal(size=(5000,))
+        folds = list(KFold(2).split(X, y))
+        model = Lasso(alpha=0.01)
+        nuisance, model_list, fitted_inds = _crossfit(Wrapper(model),
+                                                      folds,
+                                                      X, y, None, W=y, Z=None)
+        np.testing.assert_allclose(nuisance[0][folds[0][1]],
+                                   model.fit(X[folds[0][0]], y[folds[0][0]]).predict(X[folds[0][1]]))
+        np.testing.assert_allclose(nuisance[0][folds[0][0]],
+                                   model.fit(X[folds[0][1]], y[folds[0][1]]).predict(X[folds[0][0]]))
+        coef_ = np.zeros(X.shape[1])
+        coef_[0] = 1
+        [np.testing.assert_allclose(coef_, mdl._model.coef_, rtol=0, atol=0.08) for mdl in model_list]
+        np.testing.assert_array_equal(fitted_inds, np.arange(X.shape[0]))
+
+        np.random.seed(123)
+        X = np.random.normal(size=(5000, 3))
+        y = X[:, 0] + np.random.normal(size=(5000,))
+        folds = list(KFold(2).split(X, y))
+        model = Lasso(alpha=0.01)
+        nuisance, model_list, fitted_inds = _crossfit(Wrapper(model),
+                                                      folds,
+                                                      X, y, None, W=None, Z=None)
+        np.testing.assert_allclose(nuisance[0][folds[0][1]],
+                                   model.fit(X[folds[0][0]], y[folds[0][0]]).predict(X[folds[0][1]]))
+        np.testing.assert_allclose(nuisance[0][folds[0][0]],
+                                   model.fit(X[folds[0][1]], y[folds[0][1]]).predict(X[folds[0][0]]))
+        coef_ = np.zeros(X.shape[1])
+        coef_[0] = 1
+        [np.testing.assert_allclose(coef_, mdl._model.coef_, rtol=0, atol=0.08) for mdl in model_list]
+        np.testing.assert_array_equal(fitted_inds, np.arange(X.shape[0]))
+
+        class Wrapper:
+
+            def __init__(self, model):
+                self._model = model
+
+            def fit(self, X, y, W=None):
+                self._model.fit(X, y)
+                return self
+
+            def predict(self, X, y, W=None):
+                return self._model.predict(X), y - self._model.predict(X), X
 
         np.random.seed(123)
         X = np.random.normal(size=(5000, 3))
