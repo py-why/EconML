90775c9c9d349276b1ae4c22010ae0345fb2894f
vasilismsr <38927647+vasilismsr@users.noreply.github.com>
2019-11-18 09:58:32 -0500

    Add flowchart to docs

--- doc/conf.py
+++ doc/conf.py
@@ -116,6 +116,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+html_extra_path = ['map.svg']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
