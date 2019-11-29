753563ace7e4591e50791605aef3ce7cd9f9cdd0
Vasileios Syrgkanis <vasy@microsoft.com>
2019-11-28 20:52:43 -0500

    added extra test related to non-auto subsample_fr

--- econml/tests/test_ensemble.py
+++ econml/tests/test_ensemble.py
@@ -30,6 +30,22 @@ class TestSubsampledHonestForest(unittest.TestCase):
             np.testing.assert_array_less(lb, X_test[:, 0] + .05)
             np.testing.assert_array_less(X_test[:, 0], ub + .05)
 
+    def test_nonauto_subsample_fr(self):
+        np.random.seed(123)
+        n = 5000
+        d = 5
+        x_grid = np.linspace(-1, 1, 10)
+        X_test = np.hstack([x_grid.reshape(-1, 1), np.random.normal(size=(10, d - 1))])
+        X = np.random.normal(0, 1, size=(n, d))
+        y = X[:, 0] + np.random.normal(0, .1, size=(n,))
+        est = SubsampledHonestForest(n_estimators=100, subsample_fr=.8, max_depth=5, min_samples_leaf=10, verbose=0)
+        est.fit(X, y)
+        point = est.predict(X_test)
+        lb, ub = est.predict_interval(X_test, alpha=0.01)
+        np.testing.assert_allclose(point, X_test[:, 0], rtol=0, atol=.2)
+        np.testing.assert_array_less(lb, X_test[:, 0] + .05)
+        np.testing.assert_array_less(X_test[:, 0], ub + .05)
+
     def test_y2d(self):
         np.random.seed(123)
         n = 5000
