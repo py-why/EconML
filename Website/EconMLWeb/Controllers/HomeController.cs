7f68cd56af1f362f9f4e5fd449e9bf8c39283031
Paul Oka <paul@oka.us>
2020-02-18 15:02:27 -0500

    Initial add for EconML website

---
+++ Website/EconMLWeb/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AliceWeb.Models;
+
+namespace AliceWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult GetStarted()
+        {
+            return View();
+        }
+        public IActionResult HowTo()
+        {
+            return View();
+        }
+        public IActionResult Publications()
+        {
+            return View();
+        }
+
+        public IActionResult CaseStudies()
+        {
+            return View();
+        }
+
+        public IActionResult News()
+        {
+            return View();
+        }
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
