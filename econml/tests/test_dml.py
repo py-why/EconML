a2b04aa74c7556541120a1e1296bc14bc50aeb13
Vasilis <vasy@microsoft.com>
2020-09-10 13:26:18 -0400

    tests for new dml coef_ and intercept_ functionality

--- econml/tests/test_dml.py
+++ econml/tests/test_dml.py
@@ -110,7 +110,16 @@ class TestDML(unittest.TestCase):
                                     all_infs.append(BootstrapInference(1))
 
                                 for est, multi, infs in\
-                                    [(LinearDMLCateEstimator(model_y=Lasso(),
+                                    [(DMLCateEstimator(model_y=Lasso(),
+                                                       model_t=model_t,
+                                                       model_final=Lasso(alpha=0.1, fit_intercept=False),
+                                                       featurizer=featurizer,
+                                                       fit_cate_intercept=fit_cate_intercept,
+                                                       discrete_treatment=is_discrete),
+                                      True,
+                                      [None] +
+                                      ([BootstrapInference(n_bootstrap_samples=20)] if not is_discrete else [])),
+                                     (LinearDMLCateEstimator(model_y=Lasso(),
                                                              model_t='auto',
                                                              featurizer=featurizer,
                                                              fit_cate_intercept=fit_cate_intercept,
@@ -171,7 +180,8 @@ class TestDML(unittest.TestCase):
                                             eff = est.effect(X, T0=T0, T1=T)
                                             self.assertEqual(shape(eff), effect_shape)
 
-                                            if isinstance(est, LinearDMLCateEstimator) or\
+                                            if isinstance(est, DMLCateEstimator) or\
+                                                    isinstance(est, LinearDMLCateEstimator) or\
                                                     isinstance(est, SparseLinearDMLCateEstimator):
                                                 self.assertEqual(shape(est.coef_), coef_shape)
                                                 if fit_cate_intercept:
@@ -190,6 +200,8 @@ class TestDML(unittest.TestCase):
                                                 self.assertEqual(shape(est.effect_interval(X, T0=T0, T1=T)),
                                                                  (2,) + effect_shape)
                                                 if (isinstance(est,
+                                                               DMLCateEstimator) or
+                                                    isinstance(est,
                                                                LinearDMLCateEstimator) or
                                                     isinstance(est,
                                                                SparseLinearDMLCateEstimator)):
@@ -268,6 +280,8 @@ class TestDML(unittest.TestCase):
 
                                                 # test coef__inference and intercept__inference
                                                 if (isinstance(est,
+                                                               DMLCateEstimator) or
+                                                    isinstance(est,
                                                                LinearDMLCateEstimator) or
                                                     isinstance(est,
                                                                SparseLinearDMLCateEstimator)):
