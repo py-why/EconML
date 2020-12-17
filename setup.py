e4ce16d0ea283ffed2df70a8e774dc4d4ccf6f8d
Keith Battocchi <kebatt@microsoft.com>
2020-12-16 19:21:19 -0500

    Enable setuptools build process

--- setup.py
+++ setup.py
@@ -1,22 +1,11 @@
-import sys
-import os
+from setuptools import setup
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+import numpy as np
 
 
-def configuration(parent_package='', top_path=None):
-    from numpy.distutils.misc_util import Configuration
-    import numpy
-
-    libraries = []
-    if os.name == 'posix':
-        libraries.append('m')
-
-    config = Configuration(None, parent_package, top_path)
-
-    config.add_subpackage('econml')
-
-    return config
-
-
-if __name__ == '__main__':
-    from numpy.distutils.core import setup
-    setup(**configuration(top_path='').todict())
+# configuration is all pulled from setup.cfg
+setup(ext_modules=cythonize([Extension("*", ["**/*.pyx"],
+                                       include_dirs=[np.get_include()])],
+                            language_level="3"),
+      zip_safe=False)
