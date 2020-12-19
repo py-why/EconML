78722b413c1b2b004f6ad40dcaf162831365317b
Vasilis <vasy@microsoft.com>
2020-12-18 19:59:13 -0500

    removed shap

--- econml/cate_estimator.py
+++ econml/cate_estimator.py
@@ -9,7 +9,6 @@ from functools import wraps
 from copy import deepcopy
 from warnings import warn
 from collections import defaultdict
-import shap
 from .inference import BootstrapInference
 from .utilities import (tensordot, ndim, reshape, shape, parse_final_model_params,
                         inverse_onehot, Summary, get_input_columns, broadcast_unit_treatments,
