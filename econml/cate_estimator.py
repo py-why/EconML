2e6a85ba33ac7e8cff0384eb459c480883faf177
Vasilis <vasy@microsoft.com>
2020-12-18 23:06:11 -0500

    retyped function

--- econml/cate_estimator.py
+++ econml/cate_estimator.py
@@ -465,30 +465,13 @@ class LinearCateEstimator(BaseCateEstimator):
         pass
 
     def shap_values(self, X, *, feature_names=None, treatment_names=None, output_names=None):
-        """ Shap value for the final stage models (const_marginal_effect)
-        Parameters
-        ----------
-        X: (m, d_x) matrix
-            Features for each sample. Should be in the same shape of fitted X in final stage.
-        feature_names: optional None or list of strings of length X.shape[1] (Default=None)
-            The names of input features.
-        treatment_names: optional None or list (Default=None)
-            The name of treatment. In discrete treatment scenario, the name should not include control name.
-        output_names:  optional None or list (Default=None)
-            The name of the outcome.
-        Returns
-        -------
-        shap_outs: nested dictionary of Explanation object
-            A nested dictionary by using each Y and each T as key and the shap_values explanation object as value.
-        """
         d_t = self._d_t[0] if self._d_t else 1
         d_y = self._d_y[0] if self._d_y else 1
         if treatment_names is None:
-            treatment_names = [f"T{i}" for i in range(d_t)]
+            treatment_names = ["T{}".format(i) for i in range(d_t)]
         if output_names is None:
-            output_names = [f"Y{i}" for i in range(d_y)]
-
-        return _shap_explain_cme(self.const_marginal_effect, X, d_y, d_t, feature_names, treatment_names, output_names)
+            output_names = ["Y{}".format(i) for i in range(d_y)]
+        return None
 
 
 class TreatmentExpansionMixin(BaseCateEstimator):
