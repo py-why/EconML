1f96a7b0f268c7da2eb0cfed6037b9a515ede255
Keith Battocchi <kebatt@microsoft.com>
2020-12-22 13:43:53 -0500

    Switch to tox

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools.extension import Extension
 from Cython.Build import cythonize
 import numpy as np
 
-
 # configuration is all pulled from setup.cfg
-setup(ext_modules=cythonize([Extension("*", ["**/*.pyx"],
+setup(ext_modules=cythonize([Extension("econml.tree.*", ["econml/tree/*.pyx"],
+                                       include_dirs=[np.get_include()]),
+                             Extension("econml.grf.*", ["econml/grf/*.pyx"],
                                        include_dirs=[np.get_include()])],
                             language_level="3"),
       zip_safe=False)
