b4351310cff66e0b32bbdbcd5655b9eb46cd43a8
Vasilis <vasy@microsoft.com>
2021-01-14 13:13:27 -0500

    small fixes

--- econml/__init__.py
+++ econml/__init__.py
@@ -2,6 +2,6 @@
 __all__ = ['automated_ml', 'bootstrap',
            'cate_interpreter', 'causal_forest',
            'data', 'deepiv', 'dml', 'drlearner', 'inference',
-           'metalearners', 'ortho_forest', 'ortho_iv',
+           'metalearners', 'ortho_forest', 'orf', 'ortho_iv',
            'score', 'sklearn_extensions', 'tree',
            'two_stage_least_squares', 'utilities']
--- econml/automated_ml/__init__.py
+++ econml/automated_ml/__init__.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 from ._automated_ml import (setAutomatedMLWorkspace, addAutomatedML,
                             AutomatedMLModel, AutomatedMLMixin, EconAutoMLConfig)
 
