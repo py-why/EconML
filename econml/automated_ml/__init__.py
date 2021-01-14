77db23e25911cbf26e8a6bb5e9813327ba304fb7
Vasilis <vasy@microsoft.com>
2021-01-14 13:12:07 -0500

    auto init missing

---
+++ econml/automated_ml/__init__.py
@@ -0,0 +1,8 @@
+from ._automated_ml import (setAutomatedMLWorkspace, addAutomatedML,
+                            AutomatedMLModel, AutomatedMLMixin, EconAutoMLConfig)
+
+__all__ = ["setAutomatedMLWorkspace",
+           "addAutomatedML",
+           "AutomatedMLModel",
+           "AutomatedMLMixin",
+           "EconAutoMLConfig"]
