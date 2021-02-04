77e7216991edd44097c179123edbc66a83fefa01
Keith Battocchi <kebatt@microsoft.com>
2021-02-04 09:39:21 -0500

    Add __version__ attribute

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@ from setuptools import setup
 from setuptools.extension import Extension
 from Cython.Build import cythonize
 import numpy as np
+import os
+import re
 
+with open(os.path.join(os.path.dirname(__file__), "econml", "__init__.py")) as file:
+    for line in file:
+        m = re.fullmatch("__version__ = '([^']+)'\n", line)
+        if m:
+            version = m.group(1)
 
 # configuration is all pulled from setup.cfg
 setup(ext_modules=cythonize([Extension("*", ["**/*.pyx"],
                                        include_dirs=[np.get_include()])],
                             language_level="3"),
-      zip_safe=False)
+      zip_safe=False,
+      version=version)
