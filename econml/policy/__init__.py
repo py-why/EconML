10122988d9f0f7436381b13eb6312f6f250ae73e
Vasilis <vasy@microsoft.com>
2021-03-19 18:25:18 -0400

    fixed imports

--- econml/policy/__init__.py
+++ econml/policy/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from .forest import PolicyTree, PolicyForest
+from ._forest import PolicyTree, PolicyForest
 from ._drlearner import DRPolicyTree, DRPolicyForest
 
 __all__ = ["PolicyTree",
