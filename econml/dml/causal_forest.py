5ab177ffd1a0a7ff8f98a4a602baa762bb91fa02
vsyrgkanis <bsyrganis@gmail.com>
2021-05-11 07:45:37 -0400

    fixed max_samples=.5 in default grid for cfddml.tune (#462)
    * fixed max_samples=.5 in default grid for cfddml.tune
    
    * relaxed rscorer test
    
    * fixed rounding versus flooring in clauclating size of subsample. Fixed tests in grf_python so that this error would have been caught.

--- econml/dml/causal_forest.py
+++ econml/dml/causal_forest.py
@@ -647,7 +647,7 @@ class CausalForestDML(_BaseDML):
         Y, T, X, W, sample_weight, groups = check_input_arrays(Y, T, X, W, sample_weight, groups)
 
         if params == 'auto':
-            params = {'max_samples': [.3, .5],
+            params = {'max_samples': [.3, .45],
                       'min_balancedness_tol': [.3, .5],
                       'min_samples_leaf': [5, 50],
                       'max_depth': [3, None],
