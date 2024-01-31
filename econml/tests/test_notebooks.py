0ed59263578c22679a884325c2156be3e41c2f72
Keith Battocchi <kebatt@microsoft.com>
2024-01-31 10:35:02 -0500

    Fix allow_errors flag
    Signed-off-by: Keith Battocchi <kebatt@microsoft.com>

--- econml/tests/test_notebooks.py
+++ econml/tests/test_notebooks.py
@@ -46,7 +46,7 @@ def test_notebook(file):
     # require all cells to complete within 15 minutes, which will help prevent us from
     # creating notebooks that are annoying for our users to actually run themselves
     ep = nbconvert.preprocessors.ExecutePreprocessor(
-        timeout=1800, ignore_errors=True, extra_arguments=["--HistoryManager.enabled=False"])
+        timeout=1800, allow_errors=True, extra_arguments=["--HistoryManager.enabled=False"])
 
     ep.preprocess(nb, {'metadata': {'path': _nbdir}})
 
