c6337cc886d134d117f6eb43f4a9a1dddbcceb63
Carl Gold <carl@offerfit.ai>
2025-01-27 13:00:08 -0800

    Do a better job of preserving the original

--- econml/dml/dml.py
+++ econml/dml/dml.py
@@ -54,18 +54,22 @@ class _FirstStageWrapper:
                 raise AttributeError("Cannot use a classifier as a first stage model when the target is continuous!")
             return self._model.predict(_combine(X, W, n_samples))
 
-    def score(self, X, W, Target, scoring='mean_squared_error', sample_weight=None):
-        if hasattr(self._model, 'score'):
-            if self._discrete_target:
-                # In this case, the Target is the one-hot-encoding of the treatment variable
-                # We need to go back to the label representation of the one-hot so as to call
-                # the classifier.
-                Target = inverse_onehot(Target)
-            XW_combined = _combine(X, W, Target.shape[0])
-            return _FirstStageWrapper._wrap_scoring(scoring,Y_true=Target, X=XW_combined, est=self._model,
-                                sample_weight=sample_weight)
+    def score(self, X, W, Target, sample_weight=None, scoring=None):
+        XW_combined = _combine(X, W, Target.shape[0])
+        if self._discrete_target:
+            # In this case, the Target is the one-hot-encoding of the treatment variable
+            # We need to go back to the label representation of the one-hot so as to call
+            # the classifier.
+            Target = inverse_onehot(Target)
+        if hasattr(self._model, 'score') and scoring is None:
+            if sample_weight is not None:
+                return self._model.score(XW_combined, Target, sample_weight=sample_weight)
+            else:
+                return self._model.score(XW_combined, Target)
         else:
-            return None
+            return _FirstStageWrapper._wrap_scoring(scoring,Y_true=Target, X=XW_combined, est=self._model,
+                            sample_weight=sample_weight)
+
     @staticmethod
     def _wrap_scoring(scoring, Y_true, X, est, sample_weight=None):
         """
