6910ed3c649fc750b6e288cbfee11e9908944ad4
Carl Gold <carl@offerfit.ai>
2025-04-28 09:25:44 -0700

    Fix name conflict between input file and result file

--- scripts/memory_test_script.py
+++ scripts/memory_test_script.py
@@ -91,12 +91,12 @@ def causalforestdml_memory_test(
     logger.info(f"Maximum memory usage: {max_memory2} MiB")
     logger.info(f"Time to fit: {elapsed_time2} seconds")
 
-    file_name = os.path.join(root_dir,"mem_test_results.csv")
-    if not os.path.isfile(file_name):
-        with open(file_name,"w") as output:
+    result_file_name = os.path.join(root_dir,"mem_test_results.csv")
+    if not os.path.isfile(result_file_name):
+        with open(result_file_name,"w") as output:
             output.write("data,N_examples,N_nuisances,N_treatments,CFDML_max_memory,CFDML_fit_time,CB_max_memory,CB_fit_time\n")
 
-    with open(file_name,"a") as output:
+    with open(result_file_name,"a") as output:
         output.write(f"{file_name},{X.shape[0]},{X.shape[1]},{T.shape[1]},{max_memory},{elapsed_time},{max_memory2},{elapsed_time2}\n")
 
 
